332366a264d488ae39228557766d12f25b28fb4d
DESKTOP-J0GN1PA\Amir Javaid <41790096+AmirJavaidSwe@users.noreply.github.com>
2022-06-15 11:22:00 +0500

    prod build

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,9 +16,6 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-    <!-- Styles -->
-    {{-- <link href="{{ mix('css/app.css') }}" rel="stylesheet"> --}}
 </head>
 <body>
     <div id="app">
